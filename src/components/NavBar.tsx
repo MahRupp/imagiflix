b2f7ff28a25f8a487adc7fda1b141bbb9213c201
MAh <mahrupp@gmail.com>
2022-04-12 21:37:11 -0300

    hero finalizado

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,60 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "../assets/imagiflix.png";
 import { faSearch, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import placeholderUser from "../assets/user.jpg";
 
+import './NavBar.css'
 
 
-const NavBar = () => (
-  <nav className='absolute top-0 left-0 grid grid-cols-2 w-full items-center p-8'>
-    <div className="justify-self-start flex items-center">
-      <h1 className="hidden">Imagifix</h1>
-      <img src={logo} alt='Imagiflix'/>
 
-      <ul className=" flex px-5 gap-4">
-        <li className="font-bold">Início</li>
-        <li>  
-          <a href="#series">Séries</a>
-        </li>
-        <li>
-          <a href="#movies">Filmes</a>
-        </li>
-      </ul>
-    </div>    
+const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    <div className="flex justify-self-end items-center">
-      <form className="relative w-64">
-        <input 
-        className="w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100" 
-        type='text' 
-        placeholder="Titulos, gente, gêneros"
-        />
-        <button className="absolute right-2 py-1">
-          <FontAwesomeIcon icon={ faSearch } />
-        </button>
-      </form>
-      <div className="ml-4 flex">
-        <img src={placeholderUser} alt='Foto do usuario'/>
-        <button>
-         <FontAwesomeIcon className="ml-3" icon={ faCaretDown } />
-        </button>
-        <ul className="hidden">
-          <li>
-            <a href='#account'>Minha conta</a>
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <nav className='navbar absolute top-0 left-0 grid grid-cols-2 w-full items-center p-8'>
+      <div className="justify-self-start flex items-center">
+        <h1 className="hidden">Imagifix</h1>
+        <img src={logo} alt='Imagiflix'/>
+
+        <ul className=" flex px-5 gap-4">
+          <li className="font-bold">Início</li>
+          <li>  
+            <a href="#series">Séries</a>
           </li>
           <li>
-            <a href='#logout'>Sair</a>
+            <a href="#movies">Filmes</a>
           </li>
         </ul>
+      </div>    
+
+      <div className="flex justify-self-end items-center">
+        <form className="relative w-64">
+          <input 
+          className="w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100" 
+          type='text' 
+          placeholder="Titulos, gente, gêneros"
+          />
+          <button className="absolute right-2 py-1" onClick={ e => e.preventDefault()}> 
+            <FontAwesomeIcon icon={ faSearch } />
+          </button>
+        </form>
+        <div className="relative ml-4 flex">
+          <img 
+            src={placeholderUser} 
+            alt='Foto do usuario'
+            onClick={handleClick}
+            />
+          <button onClick={handleClick}>
+            <FontAwesomeIcon 
+            className="ml-3" 
+            icon={ faCaretDown } 
+            />
+          </button>
+          <ul className={`absolute mt-10 top-0 right-0 w-32 bg-black rounded p-4 transition-all duration-500 ease-in-out ${!isMenuOpen && 'opacity-0 invisible'}`}>
+            <li>
+              <a href='#account' onClick={handleClick}>Minha conta</a>
+            </li>
+            <li>
+              <a href='#logout' onClick={handleClick}>Sair</a>
+            </li>
+          </ul>
+        </div>
+        
+
       </div>
-      
-
-    </div>
-  </nav>
-
-);
+    </nav>
+  );
+};
 
 
 
